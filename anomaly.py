dc543d66975235a2a4f03a14b21dae1d57d136cd
Daniel Hogan <6313241+dphogan@users.noreply.github.com>
2023-06-03 21:01:06 -0400

    File to hold class

---
+++ anomaly.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class anomaly:
+    """
+    Computer vision anomaly detection
+    Uses algorithm from: Napoletano, Piccoli, and Schettini,
+    Anomaly detection in nanofibrous materials by CNN-based self-similarity.
+    Sensors, 2018, 209.
+    """
+    pass
